11dfc770cd9f1aeaed4fefbac2b1555d4cc20f83
denis <denisviskov16@gmail.com>
2022-09-30 12:57:34 +0300

    test

--- src/main/java/com/example/minio_example/MinioExampleApplication.java
+++ src/main/java/com/example/minio_example/MinioExampleApplication.java
@@ -1,13 +1,23 @@
 package com.example.minio_example;
 
+import io.minio.MinioClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MinioExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MinioExampleApplication.class, args);
+    }
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("minioadmin", "minioadmin")
+                .build();
     }
 
 }
--- src/test/java/com/example/minio_example/MinioExampleApplicationTests.java
+++ src/test/java/com/example/minio_example/MinioExampleApplicationTests.java
@@ -1,13 +1,71 @@
 package com.example.minio_example;
 
+import io.minio.BucketExistsArgs;
+import io.minio.GetObjectArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.RemoveObjectArgs;
+import io.minio.UploadObjectArgs;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.file.Path;
 
 @SpringBootTest
 class MinioExampleApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private MinioClient minioClient;
+    @TempDir
+    private Path tempDir;
+    private static final String BUCKET_NAME = "test-bucket";
+    private static final String TEST_OBJECT_NAME = "test_object.txt";
+
+    @BeforeEach
+    void setUp() throws Exception {
+        var bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(BUCKET_NAME).build());
+        if (!bucketExists) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(BUCKET_NAME).build());
+        }
     }
 
+    @Test
+    void example() throws Exception {
+        var testFilePath = tempDir.resolve("testFile.txt");
+        boolean created = testFilePath.toFile().createNewFile();
+
+        if (created) {
+            minioClient.uploadObject(
+                    UploadObjectArgs.builder()
+                            .bucket(BUCKET_NAME)
+                            .object(TEST_OBJECT_NAME)
+                            .filename(testFilePath.toString())
+                            .build()
+            );
+        }
+
+        var object = minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(BUCKET_NAME)
+                        .object(TEST_OBJECT_NAME)
+                        .build()
+        );
+
+        String res = object.object();
+        System.out.println(res);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        minioClient.removeObject(
+                RemoveObjectArgs.builder()
+                        .bucket(BUCKET_NAME)
+                        .object(TEST_OBJECT_NAME)
+                        .build()
+        );
+    }
 }

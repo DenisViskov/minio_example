a441c4b6d6ba66231c6eb22d8def54077c8cc9a7
denis <denisviskov16@gmail.com>
2022-09-30 11:45:41 +0300

    Initial commit

---
+++ src/test/java/com/example/minio_example/MinioExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minio_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
